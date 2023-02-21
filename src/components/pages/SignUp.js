4aa44368c27d46255b9c84d6146a9e5e3bcbdee5
developerjame <119099522+developerjame@users.noreply.github.com>
2023-02-21 10:33:23 +0300

    world tour

---
+++ src/components/pages/SignUp.js
@@ -0,0 +1,19 @@
+import React, {useState} from "react";
+import { Login } from "./Login";
+import { Register } from './Register';
+
+function SignUp() {
+  const [currentForm, setCurrentForm] = useState('login');
+  const toggleForm = (forName) =>{
+    setCurrentForm(forName);
+  }
+  return (
+    <div className="App">
+      {
+        currentForm === "login" ? <Login onFormSwitch={toggleForm}/>:<Register onFormSwitch={toggleForm}/>
+      }
+    </div>
+  );
+}
+
+export default SignUp;
